a99dfa83422522b37e54fd702884e827fb1795a3
Treborsky <rob.koziarski2k@gmail.com>
2022-08-15 18:33:08 +0200

    removing makefiles, will use something custom

--- src/phone_number/inc/phone_number.h
+++ src/phone_number/inc/phone_number.h
@@ -2,8 +2,13 @@
 #ifndef PHONEBOOK_PHONE_NUMBER_H_
 #define PHONEBOOK_PHONE_NUMBER_H_
 
-class PhoneNumber {
+#include <string>
 
+class PhoneNumber {
+public:
+  PhoneNumber() : phone_number_("no phone number") {}
+private:
+  std::string phone_number_;
 };
 
 #endif // PHONEBOOK_PHONE_NUMBER_H_
